a51c21aab734702a89e9fe3162da6bd447267845
Akshay Lavate <79072897+Akshay1299@users.noreply.github.com>
2024-05-25 10:12:44 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/You and your books/you-and-your-books.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+//#include<Windows.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    /*You are requried to complete this method */
+    long long max_Books(int a[], int n, int k) {
+        // Your code here
+        long long mx{}, x{};
+        
+        for(int i{}; i < n; i++)
+            if(a[i] > k) x = 0;
+            else if(a[i] <= k) x += a[i], mx = max(mx, x);
+        
+        return mx;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        int ar[n];
+        for (int i = 0; i < n; i++) {
+            cin >> ar[i];
+        }
+        Solution ob;
+        cout << ob.max_Books(ar, n, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
