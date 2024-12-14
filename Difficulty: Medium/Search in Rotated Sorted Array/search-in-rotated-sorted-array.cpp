3475af5abafcedfdb7107fdfb5e7c0f78747abe0
Akshay Lavate <79072897+Akshay1299@users.noreply.github.com>
2024-12-14 11:03:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
@@ -6,17 +6,56 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
+    int pivotIndex(vector<int> arr)
+    {
+        int l=0,h=arr.size()-1;
+        int ans=-1;
+        
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            
+            if(arr[mid] < arr[mid+1] and arr[mid] < arr[mid-1]) return mid;
+            else if(arr[mid] > arr[h]) l=mid+1;
+            else
+                h=mid-1;
+        }
+        
+        return ans;
+    }
+    
+    int binarySearch(vector<int>& arr, int key,int l,int h)
+    {
+        //int l=0,h=arr.size();
+        int ans=-1;
+        
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            
+            if(arr[mid] == key) return mid;
+            else if(arr[mid] > key) h=mid-1;
+            else
+                l=mid+1;
+        }
+        
+        return ans;
+    }
     int search(vector<int>& arr, int key) {
         // complete the function here
-         int i,l;
-        l=arr.size();
-
-        for (i=0;i<l;i++){
-            if(arr[i]==key){
-                return i;
-            }
+        
+        int rotated_ind=pivotIndex(arr);
+        int ind;
+        //cout<<"rotated_ind:"<<rotated_ind<<'\n';
+        if(rotated_ind!=-1)
+        {
+            ind=binarySearch(arr,key,0,rotated_ind-1);
+            if(ind!=-1) return ind;
+            else
+                return binarySearch(arr,key,rotated_ind,arr.size()-1);
         }
-        return -1;
+        else
+        return binarySearch(arr,key,0,arr.size()-1);
     }
 };
 
@@ -38,6 +77,7 @@ int main() {
         cin >> key;
         Solution ob;
         cout << ob.search(arr, key) << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
