f3cb9358c7d1d172c36bc35c81032be95c39570d
Akshay Lavate <79072897+Akshay1299@users.noreply.github.com>
2025-03-10 22:38:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Edit Distance/edit-distance.cpp
+++ Difficulty: Hard/Edit Distance/edit-distance.cpp
@@ -1,51 +1,49 @@
 //{ Driver Code Starts
+// Initial Template for C++
 #include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
+
 class Solution {
   public:
-    int editDistance(string str1, string str2) {
-        int m = str1.size();
-        int n = str2.size();
-        
-         vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-        
-        for(int i = 0; i <= m; i++){
-            for(int j = 0; j <= n; j++){
-                if(i == 0){
-                    dp[i][j] = j;
-                }
-                
-                else if(j == 0){
-                    dp[i][j] = i;
-                }
-                
-                else if(str1[i-1] == str2[j-1]){
-                    dp[i][j] = dp[i-1][j-1];
-                }
-                
-                else{
-                    dp[i][j] = 1 + min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]});
-                }
-            }
-        }
-        
-        return dp[m][n];
+    // Function to compute the edit distance between two strings
+    int dp[1005][1005];
+    int fn(string& s1, string& s2, int i , int j){
+        if(j==s2.size())return s1.size()-i;
+        if(i==s1.size())return s2.size()-j;
+        if(dp[i][j]!=-1)return dp[i][j];
+        int ans=1e5;
+        if(s1[i]==s2[j])ans=min(ans,fn(s1,s2,i+1,j+1));
+        ans=min(ans,fn(s1,s2,i+1,j)+1);
+        ans=min(ans,fn(s1,s2,i,j+1)+1);
+        ans=min(ans,fn(s1,s2,i+1,j+1)+1);
+        return dp[i][j]=ans;
+    }
+    int editDistance(string& s1, string& s2) {
+        memset(dp,-1,sizeof(dp));
+       return fn(s1,s2,0,0);
     }
 };
 
+
 //{ Driver Code Starts.
+
 int main() {
+
     int T;
     cin >> T;
+    cin.ignore();
     while (T--) {
-        string s, t;
-        cin >> s >> t;
+        string s1;
+        getline(cin, s1);
+        string s2;
+        getline(cin, s2);
         Solution ob;
-        int ans = ob.editDistance(s, t);
+        int ans = ob.editDistance(s1, s2);
         cout << ans << "\n";
+        cout << "~" << endl;
     }
     return 0;
 }
